7f2395c1bee3f036a57b58b771d797e3c82440f8
Bloodyaugust <greysonrichey@gmail.com>
2021-01-04 20:29:52 -0700

    Improve zoom behavior

--- addons/carmel4a97.RTS_Camera2D/RTS-Camera2D.gd
+++ addons/carmel4a97.RTS_Camera2D/RTS-Camera2D.gd
@@ -42,9 +42,8 @@ var camera_zoom = get_zoom()
 # to move a view.
 export (int) var camera_margin = 50
 
-# It changes a camera zoom value in units... (?, but it works... it probably
-# multiplies camera size by 1+camera_zoom_speed)
-const camera_zoom_speed = Vector2(0.5, 0.5)
+export var zoom_speed: float
+export var zoom_min: float
 
 # Vector of camera's movement / second.
 var camera_movement = Vector2()
@@ -60,84 +59,77 @@ var __rmbk = false
 var __keys = [false, false, false, false]
 
 func _ready():
-	set_h_drag_enabled(false)
-	set_v_drag_enabled(false)
-	set_enable_follow_smoothing(true)
-	set_follow_smoothing(4)
+  set_h_drag_enabled(false)
+  set_v_drag_enabled(false)
+  set_enable_follow_smoothing(true)
+  set_follow_smoothing(4)
 
 func _physics_process(delta):
-	
-	# Move camera by keys defined in InputMap (ui_left/top/right/bottom).
-	if key:
-		if __keys[0]:
-			camera_movement.x -= camera_speed * delta
-		if __keys[1]:
-			camera_movement.y -= camera_speed * delta
-		if __keys[2]:
-			camera_movement.x += camera_speed * delta
-		if __keys[3]:
-			camera_movement.y += camera_speed * delta
-	
-	# Move camera by mouse, when it's on the margin (defined by camera_margin).
-	if edge:
-		var rec = get_viewport().get_visible_rect()
-		var v = get_local_mouse_position() + rec.size/2
-		if rec.size.x - v.x <= camera_margin:
-			camera_movement.x += camera_speed * delta
-		if v.x <= camera_margin:
-			camera_movement.x -= camera_speed * delta
-		if rec.size.y - v.y <= camera_margin:
-			camera_movement.y += camera_speed * delta
-		if v.y <= camera_margin:
-			camera_movement.y -= camera_speed * delta
-	
-	# When RMB is pressed, move camera by difference of mouse position
-	if drag and __rmbk:
-		camera_movement = _prev_mouse_pos - get_local_mouse_position()
-	
-	# Update position of the camera.
-	position += camera_movement * get_zoom()
-	
-	# Set camera movement to zero, update old mouse position.
-	camera_movement = Vector2(0,0)
-	_prev_mouse_pos = get_local_mouse_position()
+  
+  # Move camera by keys defined in InputMap (ui_left/top/right/bottom).
+  if key:
+    if __keys[0]:
+      camera_movement.x -= camera_speed * delta
+    if __keys[1]:
+      camera_movement.y -= camera_speed * delta
+    if __keys[2]:
+      camera_movement.x += camera_speed * delta
+    if __keys[3]:
+      camera_movement.y += camera_speed * delta
+  
+  # Move camera by mouse, when it's on the margin (defined by camera_margin).
+  if edge:
+    var rec = get_viewport().get_visible_rect()
+    var v = get_local_mouse_position() + rec.size/2
+    if rec.size.x - v.x <= camera_margin:
+      camera_movement.x += camera_speed * delta
+    if v.x <= camera_margin:
+      camera_movement.x -= camera_speed * delta
+    if rec.size.y - v.y <= camera_margin:
+      camera_movement.y += camera_speed * delta
+    if v.y <= camera_margin:
+      camera_movement.y -= camera_speed * delta
+  
+  # When RMB is pressed, move camera by difference of mouse position
+  if drag and __rmbk:
+    camera_movement = _prev_mouse_pos - get_local_mouse_position()
+  
+  # Update position of the camera.
+  position += camera_movement * get_zoom()
+  
+  # Set camera movement to zero, update old mouse position.
+  camera_movement = Vector2(0,0)
+  _prev_mouse_pos = get_local_mouse_position()
 
 func _unhandled_input( event ):
-	if event is InputEventMouseButton:
-		if drag and\
-		   event.button_index == BUTTON_RIGHT:
-			# Control by right mouse button.
-			if event.pressed: __rmbk = true
-			else: __rmbk = false
-		# Check if mouse wheel was used. Not handled by ImputMap!
-		if wheel:
-			# Checking if future zoom won't be under 0.
-			# In that cause engine will flip screen.
-			if event.button_index == BUTTON_WHEEL_UP and\
-			camera_zoom.x - camera_zoom_speed.x > 0 and\
-			camera_zoom.y - camera_zoom_speed.y > 0:
-				camera_zoom -= camera_zoom_speed
-				set_zoom(camera_zoom)
-				# Checking if future zoom won't be above zoom_out_limit.
-			if event.button_index == BUTTON_WHEEL_DOWN and\
-			camera_zoom.x + camera_zoom_speed.x < zoom_out_limit and\
-			camera_zoom.y + camera_zoom_speed.y < zoom_out_limit:
-				camera_zoom += camera_zoom_speed
-				set_zoom(camera_zoom)
-	# Control by keyboard handled by InpuMap.
-	if event.is_action_pressed("ui_left"):
-		__keys[0] = true
-	if event.is_action_pressed("ui_up"):
-		__keys[1] = true
-	if event.is_action_pressed("ui_right"):
-		__keys[2] = true
-	if event.is_action_pressed("ui_down"):
-		__keys[3] = true
-	if event.is_action_released("ui_left"):
-		__keys[0] = false
-	if event.is_action_released("ui_up"):
-		__keys[1] = false
-	if event.is_action_released("ui_right"):
-		__keys[2] = false
-	if event.is_action_released("ui_down"):
-		__keys[3] = false
+  if event is InputEventMouseButton:
+    if drag and\
+       event.button_index == BUTTON_RIGHT:
+      # Control by right mouse button.
+      if event.pressed: __rmbk = true
+      else: __rmbk = false
+    # Check if mouse wheel was used. Not handled by ImputMap!
+    if wheel:
+      if event.button_index == BUTTON_WHEEL_UP:
+        camera_zoom = Vector2(clamp(camera_zoom.x - zoom_speed, zoom_min, zoom_out_limit), clamp(camera_zoom.y - zoom_speed, zoom_min, zoom_out_limit))
+        set_zoom(camera_zoom)
+      if event.button_index == BUTTON_WHEEL_DOWN:
+        camera_zoom = Vector2(clamp(camera_zoom.x + zoom_speed, zoom_min, zoom_out_limit), clamp(camera_zoom.y + zoom_speed, zoom_min, zoom_out_limit))
+        set_zoom(camera_zoom)
+  # Control by keyboard handled by InpuMap.
+  if event.is_action_pressed("ui_left"):
+    __keys[0] = true
+  if event.is_action_pressed("ui_up"):
+    __keys[1] = true
+  if event.is_action_pressed("ui_right"):
+    __keys[2] = true
+  if event.is_action_pressed("ui_down"):
+    __keys[3] = true
+  if event.is_action_released("ui_left"):
+    __keys[0] = false
+  if event.is_action_released("ui_up"):
+    __keys[1] = false
+  if event.is_action_released("ui_right"):
+    __keys[2] = false
+  if event.is_action_released("ui_down"):
+    __keys[3] = false
