d41fad984600e45f1d42e6679b0932c3046ce22c
Bloodyaugust <greysonrichey@gmail.com>
2021-01-05 19:53:30 -0700

    Scale boid target weight over time

--- behaviors/boid/boid.gd
+++ behaviors/boid/boid.gd
@@ -1,14 +1,16 @@
 extends Node
 
-const BOID_ENEMY_CHASE_SCALAR = 3
-const BOID_ENEMY_REPEL_SCALAR = 5
-const BOID_PLATFORM_REPEL_SCALAR = 10
-const BOID_FRIENDLY_REPEL_SCALAR = 0.25
-const BOID_MAX_CHASE_DISTANCE = 15000
-const BOID_MAX_COHESION_DISTANCE = 10000
-const BOID_MAX_ENEMY_REPEL_DISTANCE = 8000
-const BOID_MAX_FRIENDLY_REPEL_DISTANCE = 500
-const BOID_MAX_PLATFORM_REPEL_DISTANCE = 4000
+const BOID_ENEMY_CHASE_SCALAR: float = 3.0
+const BOID_ENEMY_REPEL_SCALAR: float = 5.0
+const BOID_FRIENDLY_REPEL_SCALAR: float = 0.25
+const BOID_PLATFORM_REPEL_SCALAR: float = 10.0
+const BOID_TARGET_TIME_TO_MAX_SCALAR: float = 7.5
+const BOID_TARGET_MAX_SCALAR: float = 2.0
+const BOID_MAX_CHASE_DISTANCE: float = 15000.0
+const BOID_MAX_COHESION_DISTANCE: float = 10000.0
+const BOID_MAX_ENEMY_REPEL_DISTANCE: float = 8000.0
+const BOID_MAX_FRIENDLY_REPEL_DISTANCE: float = 500.0
+const BOID_MAX_PLATFORM_REPEL_DISTANCE: float = 4000.0
 
 export var chase_enemies: bool
 
@@ -17,9 +19,11 @@ onready var _tree := get_tree()
 
 var _rotation_vector_target: Vector2
 var _target: Vector2
+var _time_following_target: float
 
 func set_target(new_target: Vector2) -> void:
   _target = new_target
+  _time_following_target = 0
 
 func _process(delta):
   var _drones = _tree.get_nodes_in_group("drones")
@@ -29,6 +33,8 @@ func _process(delta):
   var _flock_enemy_repel_vector = Vector2()
   var _flock_friendly_repel_vector = Vector2()
   var _flock_platform_repel_vector = Vector2()
+
+  _time_following_target += delta
 
   for _drone in _drones:
     if _drone != _parent:
@@ -46,7 +52,7 @@ func _process(delta):
             _flock_enemy_repel_vector += (_parent.global_position - _drone.global_position).normalized()
   _flock_rotation_vector = _flock_rotation_vector.normalized()
 
-  _rotation_vector_target = (_target - _parent.global_position).normalized()
+  _rotation_vector_target = (_target - _parent.global_position).normalized() * lerp(1, BOID_TARGET_MAX_SCALAR, _time_following_target / BOID_TARGET_TIME_TO_MAX_SCALAR)
 
   for _platform in _platforms:
     if _platform.global_position.distance_squared_to(_parent.global_position) < BOID_MAX_PLATFORM_REPEL_DISTANCE:
